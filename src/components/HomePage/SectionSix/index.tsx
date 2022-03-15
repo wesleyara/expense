690945f4c1e623f5680f45b15c16322838c2dbcc
wesleyara <wes1gom@gmail.com>
2022-03-15 14:57:38 -0300

    feat: adding aboutus components

---
+++ src/components/HomePage/SectionSix/index.tsx
@@ -0,0 +1,46 @@
+import { GrCloudComputer } from "react-icons/gr";
+import { DiJavascript1 } from "react-icons/di";
+import { FaMobileAlt } from "react-icons/fa";
+
+import { SFeatures, SHome } from "./style";
+
+export function SectionSix() {
+  return (
+    <>
+      <SHome id="aboutUs">
+        <h1>The Product we work with.</h1>
+        <span>
+          Everything we developed it was made for you. See each of our features,
+          designed exclusively for your best experience using our product.
+        </span>
+      </SHome>
+      <SFeatures>
+        <div className="cardFeatures">
+          <div
+            style={{
+              backgroundColor: "rgba(0, 255, 255, 0.2)",
+            }}
+          >
+            <GrCloudComputer />
+          </div>
+          <h2>Storage</h2>
+          <span>Data stored on local storage</span>
+        </div>
+        <div className="cardFeatures">
+          <div style={{ backgroundColor: "rgba(255, 255, 0, 0.3)" }}>
+            <DiJavascript1 />
+          </div>
+          <h2>JavaScript</h2>
+          <span>Made with one of the most famous programming languages</span>
+        </div>
+        <div className="cardFeatures">
+          <div style={{ backgroundColor: "rgba(128, 0, 128, 0.3)" }}>
+            <FaMobileAlt />
+          </div>
+          <h2>Cross plataform</h2>
+          <span>Access on any device</span>
+        </div>
+      </SFeatures>
+    </>
+  );
+}
---
+++ src/components/HomePage/SectionSix/style.ts
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+export const SHome = styled.section`
+  width: 80%;
+  margin: 80px auto;
+  display: flex;
+
+  h1 {
+    font-size: 6rem;
+    line-height: 6.2rem;
+    flex: 2;
+  }
+
+  span {
+    display: inline-block;
+    color: #bdbdbd;
+    font-size: 23px;
+    margin-top: 20px;
+    flex: 1;
+  }
+`;
+
+export const SFeatures = styled.div`
+  width: 80%;
+  margin: 50px auto;
+  display: flex;
+  justify-content: space-around;
+
+  .cardFeatures {
+    width: 288px;
+    text-align: center;
+    padding: 70px 50px 70px 50px;
+    background-color: rgba(0, 0, 0, 0.02);
+    border-radius: 5px;
+    cursor: pointer;
+    transition: 0.3s ease-in-out;
+
+    div {
+      padding: 10px;
+      display: inline-block;
+      overflow: hidden;
+      border-radius: 50%;
+
+      svg {
+        display: flex;
+        font-size: 35px;
+      }
+    }
+
+    span {
+      color: #bdbdbd;
+    }
+
+    &:hover {
+      transform: scale(1.1);
+    }
+  }
+`;
