6cca8197cea6933e52b45b990fa4cee4b40974c1
wesleyara <wes1gom@gmail.com>
2022-03-15 14:57:14 -0300

    feat: adding components

--- src/components/HomePage/index.tsx
+++ src/components/HomePage/index.tsx
@@ -1,9 +1,19 @@
+import { SectionFive } from "./SectionFive";
+import { SectionFour } from "./SectionFour";
 import { SectionOne } from "./SectionOne";
+import { SectionSeven } from "./SectionSeven";
+import { SectionSix } from "./SectionSix";
+import { SectionThree } from "./SectionThree";
 
 export function HomePage() {
   return (
     <>
       <SectionOne />
+      <SectionThree />
+      <SectionFour />
+      <SectionFive />
+      <SectionSix />
+      <SectionSeven />
     </>
   );
 }
