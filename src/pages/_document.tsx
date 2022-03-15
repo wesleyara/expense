3a57447a62e6568534cc50bee670ab309e493eee
wesleyara <wes1gom@gmail.com>
2022-03-15 11:09:14 -0300

    feat: adding font family

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="pt-br">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Hind+Siliguri&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
