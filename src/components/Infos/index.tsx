fa1ea248a090de19edcbf930e872c32dd48f5918
wesleyara <wes1gom@gmail.com>
2022-03-17 16:21:12 -0300

    feat: adding infos component

---
+++ src/components/Infos/index.tsx
@@ -0,0 +1,25 @@
+import { useUser } from "../../hooks/useUser";
+import { SInfos } from "./style";
+
+export function Infos() {
+  const { user, setUser } = useUser();
+
+  return (
+    <SInfos>
+      {user && (
+        <>
+          <span>Salary: R$ {user.salary.toLocaleString()}</span>
+
+          <span>
+            <ul>
+              <li>Count: {user.percentCount}%</li>
+              <li>Free: {user.percentFree}%</li>
+              <li>Savings: {user.percentSavings}%</li>
+            </ul>
+            <button onClick={() => setUser(undefined)}>Editar</button>
+          </span>
+        </>
+      )}
+    </SInfos>
+  );
+}
---
+++ src/components/Infos/style.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+export const SInfos = styled.section`
+  width: 80%;
+  max-width: 1100px;
+  padding: 1rem 0;
+  margin: 0 auto;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+
+  border-bottom: 1px solid #e7e7e7;
+  border-top: 1px solid #e7e7e7;
+
+  span {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    gap: 15px;
+
+    ul {
+      list-style: none;
+      display: flex;
+      gap: 16px;
+    }
+
+    button {
+      font-family: "Hind Siliguri", sans-serif;
+      font-size: 16px;
+      padding: 0px 20px 0px 20px;
+      background-color: #ff7235;
+      border: 1px solid #ff7235;
+      border-radius: 5px;
+      color: white;
+      cursor: pointer;
+
+      &:hover {
+        background-color: white;
+        color: #ff7235;
+      }
+    }
+  }
+`;
