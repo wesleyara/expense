f85f5a03bcbb130d4fa831b8fd8f2865cf29f7dc
wesleyara <wes1gom@gmail.com>
2022-03-17 16:22:18 -0300

    feat: adding components on the page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,9 +1,14 @@
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { Infos } from "../../components/Infos";
+import { Stats } from "../../components/Stats";
 import { LandingSalary } from "../../components/LandingSalary";
 import { useUser } from "../../hooks/useUser";
 import { NavBar } from "../../layout/NavBar";
 
 export default function Dashboard() {
+  const [home, setHome] = useState(false);
+
   const router = useRouter();
 
   function handleNavigate() {
@@ -12,20 +17,31 @@ export default function Dashboard() {
 
   const { user } = useUser();
 
+  useEffect(() => {
+    if (matchMedia("(max-width: 650px)").matches) {
+      setHome(true);
+    }
+  }, []);
+
   return (
     <>
       {!user ? (
         <LandingSalary />
       ) : (
-        <NavBar
-          listOne="Overview"
-          listTwo="List"
-          linkOne="/dashboard"
-          linkTwo="/dashboard/list"
-          linkThree="/"
-          buttonDescriber="Home"
-          eventClick={handleNavigate}
-        />
+        <>
+          <NavBar
+            listOne="Overview"
+            listTwo="List"
+            listThree={`${home == true ? "Home" : ""}`}
+            linkOne="/dashboard"
+            linkTwo="/dashboard/list"
+            linkThree="/"
+            buttonDescriber="Home"
+            eventClick={handleNavigate}
+          />
+          <Infos />
+          <Stats />
+        </>
       )}
     </>
   );
