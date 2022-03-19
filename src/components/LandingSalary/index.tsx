b420aa02c1fa969d75a56cfd8fc3103789e5f626
wesleyara <wes1gom@gmail.com>
2022-03-19 20:29:01 -0300

    refactor: change form redirect and adding modal

--- src/components/LandingSalary/index.tsx
+++ src/components/LandingSalary/index.tsx
@@ -4,7 +4,7 @@ import Loadings from "../Loading";
 import { SHome } from "./style";
 import ilustration from "../../assets/images/ilustration-secfive.svg";
 
-import { useHistory } from "react-router-dom";
+import { NewError } from "../NewError";
 
 export default function LandingSalary() {
   const [salary, setSalary] = useState(0);
@@ -12,16 +12,20 @@ export default function LandingSalary() {
   const [free, setFree] = useState(0);
   const [savings, setSavings] = useState(0);
 
+  const [newError, setNewError] = useState(false);
   const [local, setLocal] = useState(false);
 
   const { setUser } = useUser();
-  const router = useHistory();
 
   function handleSetData(e: FormEvent) {
     e.preventDefault();
 
     if (count + free + savings < 100) {
-      alert("A soma dos valores de porcentagem devem ser igual a 100.");
+      setNewError(true);
+
+      setTimeout(() => {
+        setNewError(false);
+      }, 4000);
       return;
     }
 
@@ -31,8 +35,6 @@ export default function LandingSalary() {
       percentFree: free,
       percentSavings: savings,
     });
-
-    router.push("/dashboard");
   }
 
   useEffect(() => {
@@ -42,76 +44,80 @@ export default function LandingSalary() {
   }, []);
 
   return (
-    <SHome>
-      {local == false ? (
-        <Loadings />
-      ) : (
-        <>
-          <article>
-            <div>
-              <span className="logo">EX</span> pense
-            </div>
-            <br />
-            <form onSubmit={handleSetData}>
-              <h2>Enter values in the fields to continue</h2>
-              <p>Salary:</p>
-              <span>
-                <button type="button">R$</button>
-                <input
-                  type="number"
-                  onChange={(e) => setSalary(Number(e.target.value))}
-                  min="1"
-                  placeholder="Enter salary value"
-                />
-              </span>
+    <>
+      <SHome>
+        {local == false ? (
+          <Loadings />
+        ) : (
+          <>
+            <article>
+              <div>
+                <span className="logo">EX</span> pense
+              </div>
+              <br />
+              <form onSubmit={handleSetData}>
+                <h2>Enter values in the fields to continue</h2>
+                <p>Salary:</p>
+                <span>
+                  <button type="button">R$</button>
+                  <input
+                    type="number"
+                    onChange={(e) => setSalary(Number(e.target.value))}
+                    min="1"
+                    placeholder="Enter salary value"
+                  />
+                </span>
 
-              <p>Count dedicated percentage:</p>
-              <span>
-                <button type="button">%</button>
-                <input
-                  type="number"
-                  onChange={(e) => setCount(Number(e.target.value))}
-                  min="0"
-                  max="100"
-                  placeholder="Enter count dedicate"
-                />
-              </span>
+                <p>Count dedicated percentage:</p>
+                <span>
+                  <button type="button">%</button>
+                  <input
+                    type="number"
+                    onChange={(e) => setCount(Number(e.target.value))}
+                    min="0"
+                    max="100"
+                    placeholder="Enter count dedicate"
+                  />
+                </span>
 
-              <p>Free dedicated percentage:</p>
-              <span>
-                <button type="button">%</button>
-                <input
-                  type="number"
-                  onChange={(e) => setFree(Number(e.target.value))}
-                  min="0"
-                  max="100"
-                  placeholder="Enter free dedicate"
-                />
-              </span>
+                <p>Free dedicated percentage:</p>
+                <span>
+                  <button type="button">%</button>
+                  <input
+                    type="number"
+                    onChange={(e) => setFree(Number(e.target.value))}
+                    min="0"
+                    max="100"
+                    placeholder="Enter free dedicate"
+                  />
+                </span>
 
-              <p>Savings dedicated percentage:</p>
+                <p>Savings dedicated percentage:</p>
+                <span>
+                  <button type="button">%</button>
+                  <input
+                    type="number"
+                    onChange={(e) => setSavings(Number(e.target.value))}
+                    min="0"
+                    max="100"
+                    placeholder="Enter savings dedicate"
+                  />
+                </span>
+                <button className="start" type="submit">
+                  Start
+                </button>
+              </form>
+            </article>
+            <figure>
               <span>
-                <button type="button">%</button>
-                <input
-                  type="number"
-                  onChange={(e) => setSavings(Number(e.target.value))}
-                  min="0"
-                  max="100"
-                  placeholder="Enter savings dedicate"
-                />
+                <img src={ilustration} alt="ilustration home" />
               </span>
-              <button className="start" type="submit">
-                Start
-              </button>
-            </form>
-          </article>
-          <figure>
-            <span>
-              <img src={ilustration} alt="ilustration home" />
-            </span>
-          </figure>
-        </>
-      )}
-    </SHome>
+            </figure>
+          </>
+        )}
+      </SHome>
+
+      {newError && <NewError />}
+    </>
   );
 }
