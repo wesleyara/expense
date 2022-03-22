72bd4c37cfc55f8c3c7077621403c946e90df996
wesleyara <wes1gom@gmail.com>
2022-03-22 16:15:59 -0300

    feat: create Lits component

---
+++ src/components/Lists/index.tsx
@@ -0,0 +1,126 @@
+import { SList } from "./style";
+import { useData } from "../../hooks/useData";
+import { useEffect } from "react";
+import { RiDeleteBinLine } from "react-icons/ri";
+
+export function Lists() {
+  const { data, setData } = useData();
+
+  useEffect(() => {
+    const dataBuy = localStorage.getItem("dataBuy");
+
+    if (dataBuy) {
+      setData(JSON.parse(dataBuy));
+    }
+  }, [setData]);
+
+  const filterCount = data.filter((val) => val.type === "count");
+  const filterFree = data.filter((val) => val.type === "free");
+  const filterSavings = data.filter((val) => val.type === "savings");
+
+  function handleRemovItem(type: string, itemID: string) {
+    const filteredRest = data.filter((val) => val.type !== type);
+    const filteredItem = data
+      .filter((val) => val.type === type)
+      .filter((obj) => obj.item !== itemID);
+
+    setData(filteredRest.concat(filteredItem));
+  }
+
+  useEffect(() => {
+    if (data) {
+      localStorage.setItem("dataBuy", JSON.stringify(data));
+    }
+  }, [data]);
+  return (
+    <SList>
+      <h1>Purchase List</h1>
+
+      {data.length > 0 ? (
+        <>
+          {" "}
+          {filterCount.length > 0 && (
+            <>
+              <h2>Count list:</h2>
+              <ul>
+                {filterCount.map((value, index) => {
+                  return (
+                    <li key={index}>
+                      {index + 1} -{" "}
+                      {value.item[0].toUpperCase() + value.item.substring(1)} in
+                      the amount of R$
+                      {value.value
+                        .toFixed(2)
+                        .replace(".", ",")
+                        .toLocaleString()}
+                      .
+                      <button
+                        onClick={() => handleRemovItem("count", value.item)}
+                      >
+                        <RiDeleteBinLine />
+                      </button>
+                    </li>
+                  );
+                })}
+              </ul>
+            </>
+          )}
+          {filterFree.length > 0 && (
+            <>
+              <h2>Free list:</h2>
+              <ul>
+                {filterFree.map((value, index) => {
+                  return (
+                    <li key={index}>
+                      {index + 1} -{" "}
+                      {value.item[0].toUpperCase() + value.item.substring(1)} in
+                      the amount of R$
+                      {value.value
+                        .toFixed(2)
+                        .replace(".", ",")
+                        .toLocaleString()}
+                      .
+                      <button
+                        onClick={() => handleRemovItem("free", value.item)}
+                      >
+                        <RiDeleteBinLine />
+                      </button>
+                    </li>
+                  );
+                })}
+              </ul>
+            </>
+          )}
+          {filterSavings.length > 0 && (
+            <>
+              <h2>Savings list:</h2>
+              <ul>
+                {filterSavings.map((value, index) => {
+                  return (
+                    <li key={index}>
+                      {index + 1} -{" "}
+                      {value.item[0].toUpperCase() + value.item.substring(1)} in
+                      the amount of R$
+                      {value.value
+                        .toFixed(2)
+                        .replace(".", ",")
+                        .toLocaleString()}
+                      .
+                      <button
+                        onClick={() => handleRemovItem("savings", value.item)}
+                      >
+                        <RiDeleteBinLine />
+                      </button>
+                    </li>
+                  );
+                })}
+              </ul>
+            </>
+          )}
+        </>
+      ) : (
+        <h3>Don&apos;t have any purchases yet.</h3>
+      )}
+    </SList>
+  );
+}
---
+++ src/components/Lists/style.ts
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+
+export const SList = styled.section`
+  width: 80%;
+  max-width: 1100px;
+  padding: 2rem 0;
+  margin: 0 auto;
+
+  h1 {
+    text-align: center;
+  }
+
+  h2 {
+    margin-top: 10px;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  li {
+    padding: 10px;
+    border: 1px solid #ff7235;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  button {
+    padding: 5px;
+    background-color: #ff7235;
+    border: 1px solid #ff7235;
+    border-radius: 5px;
+    color: white;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    svg {
+      color: white;
+    }
+
+    &:hover {
+      background-color: white;
+      color: #ff7235;
+
+      svg {
+        color: #ff7235;
+      }
+    }
+  }
+
+  h3 {
+    text-align: center;
+    margin-top: 30px;
+  }
+`;
